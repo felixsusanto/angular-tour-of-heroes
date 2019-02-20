c5fd265474743c83407ac77abcd4d238560b806a
Felix Susanto <felix.susanto2000@gmail.com>
2019-02-20 08:05:49 +0800

    WIP, until getHero

---
+++ src/app/hero.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
+  
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  
+  getHeroes(): Observable<Hero[]> {
+    this.log('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      )
+    ;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.log(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
